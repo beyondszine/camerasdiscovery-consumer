d0e67b8a4e6135b3a85dd93acaa00ff345463c0d
beyondszine <saurabhshandilya.1991@gmail.com>
2019-03-14 21:34:16 +0530

    promisified intial checks & config checks

--- ffmpeg-consumer.js
+++ ffmpeg-consumer.js
@@ -3,15 +3,29 @@
 
   const appConfig = require('config');
   const cDebug = require('debug')('consumer');
-  const redisURI = `${appConfig.storage.type}://${appConfig.storage.host}:${appConfig.storage.port}`;
-  cDebug("defautl config params are:",appConfig);
+  const redisURI = `${appConfig.QueueOptions.redis.type}://${appConfig.QueueOptions.redis.host}:${appConfig.QueueOptions.redis.port}`;
+  cDebug("default config params are:",appConfig);
   cDebug(`redis URI formed is ${redisURI}`);
-
   const Queue = require(appConfig.jobManager);
   const ffmpegJobsQueue = new Queue(appConfig.queueName, redisURI);
 
-  // ffmpegJobsQueue.process('VideoJobRunner','/home/beyond/nodejs/bulltry/videosaver.js');
+  var checkConfigurationValidity = new Promise(function(resolve,reject){
+    resolve();
+  });
 
+  var checkStorageValidity = new Promise(function(resolve,reject){
+    resolve();
+  });
+
+  Promise.all([checkConfigurationValidity,checkStorageValidity])
+  .then(() => {
+    cDebug("All config checks have passed! Starting to Consume given Queue Now!");
+  })
+  .catch(err => {
+    cDebug("Error occured during validation checks!",err);
+    process.exit();
+  });
+  // ffmpegJobsQueue.process('VideoJobRunner','/home/beyond/nodejs/bulltry/videosaver.js');
 
   ffmpegJobsQueue.on('completed', (job,result) => {
     cDebug(`Job named ${job.name} with Job ID ${job.id} completed with result as ${result}`);
